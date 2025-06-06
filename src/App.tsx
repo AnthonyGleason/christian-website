e31c7492aa7aa8953641c318c9cd03c51661e0bd
Anthony <ainfortunio1@gmail.com>
2025-06-05 22:35:21 -0400

    add content

--- src/App.tsx
+++ src/App.tsx
@@ -136,7 +136,7 @@ function App() {
             <img alt='my weekly newsletter' src={newsletterImg} />
             <h2>Like What You Heard?</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            {/* the below input box breaks with bitwarden, permission denied error */}
+            { /* the below input box breaks with bitwarden, permission denied error */}
             <input type='text' placeholder='Enter your email' /> 
             <Button>SUBSCRIBE</Button>
           </form>
